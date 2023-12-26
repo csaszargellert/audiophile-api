234987b1e6de5cd3cd6ae2f9cb2df26843b65f49
Gellért Császár <csaszargellert@gmail.com>
2023-12-26 09:40:56 +0100

    make client url hidden in config.env file

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const AppError = require("./utils/AppError");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "50mb" }));
